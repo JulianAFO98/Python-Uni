125df1d226035f46a94ef3c30c6e03a153092eab
julian <gonna1560@gmail.com>
2025-10-13 21:21:08 -0300

    finalizado ej 20

--- Guia_4/shanon_huffman.py
+++ Guia_4/shanon_huffman.py
@@ -1,5 +1,6 @@
 
 import math
+from operator import itemgetter
 
 def info(val,base): 
     if val == 0:
@@ -8,8 +9,6 @@ def info(val,base):
 
 def generarListaInfo(lista,base):
     return [info(val,base) for val in lista]
-
-
 
 
 def generar_lista_longitudes(palabras):
@@ -75,10 +74,10 @@ def calcularRedundanciaYEficiencia(probs_extension, palabras_codigo):
     # Redundancia y eficiencia
     eficiencia = H / L if L != 0 else 0
     R = 1 - eficiencia
-    return round(R,3), round(eficiencia,3)
+    return round(R,4), round(eficiencia,4)
 
 
-def generarListaParalelas(cadena):
+def generarListaParalelasCadenaCaracteresYProbs(cadena):
     contApariciones = []
     letras = []
     for i in range(len(cadena)):
@@ -89,8 +88,152 @@ def generarListaParalelas(cadena):
             index = letras.index(cadena[i])
             contApariciones[index]+=1
     
-    probabilidades = [round(val/len(cadena),2) for val in contApariciones]
+    probabilidades = [round(val/len(cadena),10) for val in contApariciones]
     return letras,probabilidades
+
+
+def algoritmo_huffman(probs):
+    items = [[p, [i]] for i, p in enumerate(probs)]
+    tabla_huffman = [""]*len(probs)
+    while len(items) > 1:
+        items = sorted(items, key=itemgetter(0),reverse=True)
+        ultimoElemento = items.pop()
+        penultimoElemento = items.pop()
+        for indice in ultimoElemento[1]:
+            tabla_huffman[indice] = "1" + tabla_huffman[indice]
+        for indice in penultimoElemento[1]:
+            tabla_huffman[indice] = "0" + tabla_huffman[indice]
+        nuevoElemento = [ultimoElemento[0] + penultimoElemento[0], ultimoElemento[1] + penultimoElemento[1]]
+        items.append(nuevoElemento)
+
+    print("Tabla huffman final ",tabla_huffman)
+    return tabla_huffman
+
+
+def shanon_fano_recursivo(items,tabla_shanon_fano):
+    if len(items) > 1:
+        items = sorted(items, key=itemgetter(0),reverse=True) #ordeno de mayor a menor
+        total = sum(item[0] for item in items) #sumo probabilidades
+        acum = 0 #acumulador de probabilidades
+        index_seccionado = 0 #indice donde se secciona la tabla
+        for i, item in enumerate(items): #busco el indice donde secciono
+            acum += item[0] 
+            if acum >= total / 2: #si el acumulado es mayor o igual a la mitad del total
+                if(i > 0 and (total/2 - (acum - item[0])) < (acum - total/2)):
+                    index_seccionado = i
+                else:
+                    index_seccionado = i + 1
+                break
+        for indice in items[:index_seccionado]: #agrego 0 y 1 a los codigos basandome en la seccion
+            for idx in indice[1]:
+                tabla_shanon_fano[idx] += "0"
+        for indice in items[index_seccionado:]:
+            for idx in indice[1]:
+                tabla_shanon_fano[idx] += "1"
+        shanon_fano_recursivo(items[:index_seccionado],tabla_shanon_fano)
+        shanon_fano_recursivo(items[index_seccionado:],tabla_shanon_fano)
+
+def algoritmo_shanon_fano(probs):
+    items = [[p, [i]] for i, p in enumerate(probs)]
+    tabla_shanon_fano = [""]*len(probs)
+    shanon_fano_recursivo(items,tabla_shanon_fano)
+    print("Tabla shanon fano final ",tabla_shanon_fano)
+    return tabla_shanon_fano
+
+#llamada a codificar_en_byteArray => codificar_en_byteArray("ABCD",["00","01","10","11"])
+def codificar_en_byteArray(mensaje_a_codificar,alfabeto_fuente,lista_cadena_caracteres):
+    relacion_alfabeto_fuente_codigo = {alfabeto_fuente[i]: lista_cadena_caracteres[i] for i in range(len(alfabeto_fuente))}
+    cadena_codificada = ""
+    for caracter in mensaje_a_codificar:
+        if caracter in relacion_alfabeto_fuente_codigo:
+            cadena_codificada += relacion_alfabeto_fuente_codigo[caracter]
+        else:
+            raise ValueError(f"El caracter '{caracter}' no está en el alfabeto fuente.")
+    #convertir cadena codificada en bytearray
+    # Asegurarse de que la longitud de la cadena sea múltiplo de 8
+    while len(cadena_codificada) % 8 != 0:
+        cadena_codificada += '0'  # Rellenar con ceros al final si es necesario
+    
+    byte_array = bytearray(int(cadena_codificada[i:i+8], 2) for i in range(0, len(cadena_codificada), 8))   
+    cadena_bits = "".join(f"{byte:08b}" for byte in byte_array)
+    return cadena_bits, byte_array
+
+
+
+
+def decodificar_de_byteArray(cadena_bits_byteArray,alfabeto_fuente,alfabeto_codigo):
+    
+    # Crear un diccionario para mapear códigos a símbolos del alfabeto fuente
+    codigo_a_simbolo = {alfabeto_codigo[i]: alfabeto_fuente[i] for i in range(len(alfabeto_fuente))}
+    
+    # Decodificar la cadena de bits
+    cadena_decodificada = ""
+    codigo_actual = ""
+    
+    for bit in cadena_bits_byteArray:
+        codigo_actual += bit
+        if codigo_actual in codigo_a_simbolo:
+            cadena_decodificada += codigo_a_simbolo[codigo_actual]
+            codigo_actual = ""
+    
+    return cadena_decodificada
+
+
+def codificar_usando_RCL(cadena):
+    if not cadena:
+        return bytearray()
+
+    tabla_letras = []
+    tabla_cantidad_apariciones = []
+
+    char_anterior = cadena[0]
+    cantidad = 1
+
+    for i in range(1, len(cadena)):
+        char_actual = cadena[i]
+
+        if char_actual == char_anterior and cantidad < 255:
+            cantidad += 1
+        else:
+            tabla_letras.append(char_anterior)
+            tabla_cantidad_apariciones.append(cantidad)
+            char_anterior = char_actual
+            cantidad = 1
+
+    tabla_letras.append(char_anterior)
+    tabla_cantidad_apariciones.append(cantidad)
+
+    print("Tabla letras:", tabla_letras)
+    print("Tabla apariciones:", tabla_cantidad_apariciones)
+
+    # crear el bytearray
+    bytearray_cadena = bytearray()
+    for letra, cantidad in zip(tabla_letras, tabla_cantidad_apariciones):
+        bytearray_cadena.append(ord(letra))
+        bytearray_cadena.append(cantidad)
+
+    return bytearray_cadena
+
+
+def calcular_comprension(cadena_alfabeto_fuente,byte_array):
+    compresion = -1
+    # Calcular el tamaño original en bits
+    tamaño_original_bits = len(cadena_alfabeto_fuente) * 8  # Asumiendo 1 byte (8 bits) por carácter
+    
+    # Calcular el tamaño comprimido en bits
+    tamaño_comprimido_bits = len(byte_array) * 8  # Cada byte tiene 8 bits
+    
+    # Calcular la compresión
+    if tamaño_comprimido_bits != 0:
+        compresion = tamaño_original_bits / tamaño_comprimido_bits
+    print("La compresion es de: ",compresion," veces")
+    return round(compresion,4)   
+
+def mostrarListaConIndices(lista):
+    for i in range(len(lista)):
+        print(f"[{i}]={lista[i]}",end="  ")
+    print()
+
 """
 print(cumple_primer_teorema_shannon([0.3,0.1,0.4,0.2],["BA","CAB","A","CBA"],1))
 print(cumple_primer_teorema_shannon([0.3,0.1,0.4,0.2],["BA","CAB","A","CBA"],2))
@@ -138,10 +281,86 @@ print(calcularRedundanciaYEficiencia(P, C3))
 C4 = ["11","001","000","10","01"]
 print(calcularRedundanciaYEficiencia(P, C4))
 """
-
+"""
 letras,probs = generarListaParalelas("ABCDABCBDCBAAABBBCBCBABADBCBABCBDBCCCAAABB")
 print("Letras ",letras) 
 print("Probs ",probs)
 letras,probs = generarListaParalelas("AOEAOEOOOOEOAOEOOEOOEOAOAOEOEUUUIEOEOEO")
 print("Letras ",letras) 
 print("Probs ",probs)
+
+"""
+#Punto 12
+#P = { 0.385, 0.154, 0.128, 0.154, 0.179 }
+"""
+P  = [0.385, 0.154, 0.128, 0.154, 0.179]
+ # uso cualquier valor para palabras_codigo de 0 y 1 para obtener la entropia en base 2
+print("Entropia ",calcular_entropia_fuente_codigo(["00","1","11","10","11"],P))
+tabla_huffman = algoritmo_huffman(P) # OJO CON LOS INDICES posible trampa para el examen
+tabla_shanon_fano = algoritmo_shanon_fano(P)
+print(calcular_longitud_media_codigo(tabla_huffman,P))
+print(calcular_longitud_media_codigo(tabla_shanon_fano,P))
+print(calcularRedundanciaYEficiencia(P, tabla_huffman))
+print(calcularRedundanciaYEficiencia(P, tabla_shanon_fano))
+"""
+"""
+#Punto 13
+letras,probs = generarListaParalelasCadenaCaracteresYProbs("58784784525368669895745123656253698989656452121702300223659")
+mostrarListaConIndices(letras)
+mostrarListaConIndices(probs)
+tabla_huffman = algoritmo_huffman(probs) 
+tabla_shanon_fano = algoritmo_shanon_fano(probs)
+print(calcular_longitud_media_codigo(tabla_huffman,probs))
+print(calcular_longitud_media_codigo(tabla_shanon_fano,probs))
+print(calcularRedundanciaYEficiencia(probs, tabla_huffman))
+print(calcularRedundanciaYEficiencia(probs, tabla_shanon_fano)) # preguntar
+"""
+#Punto 15
+"""
+
+cadena_bits,byte_array = codificar_en_byteArray("DDDDDDDDDBBBBBBBBBBBBBBBBBBBBBBBBAAABBCD",["00","11","10","01"]) # deberia devolver bytearray(b'\x00\x01\x02\x03\x00\x01\x02\x03')
+print(cadena_bits)
+print(byte_array)
+cadena_decodificada = decodificar_de_byteArray(cadena_bits,"BDAC",["00","11","10","01"]) #Ojo con el orden de los alfabetos
+print(cadena_decodificada)
+"""
+#Punto 16
+
+#print(calcular_comprension("DDDDDDDDDBBBBBBBBBBBBBBBBBBBBBBBBAAABBCD",byte_array))
+
+#punto 17
+"""
+
+
+simbolos = [
+    " ", ",", ".", ":", ";", "A", "B", "C",
+    "D", "E", "F", "G", "H", "I", "J", "K",
+    "L", "M", "N", "Ñ", "O", "P", "Q", "R",
+    "S", "T", "U", "V", "W", "X", "Y", "Z"
+]
+
+probs = [
+    0.175990, 0.014093, 0.015034, 0.000542, 0.002109, 0.111066, 0.015368, 0.030176,
+    0.038747, 0.101604, 0.004873, 0.008762, 0.007953, 0.049740, 0.003706, 0.000034,
+    0.048149, 0.021041, 0.050490, 0.002018, 0.073793, 0.019583, 0.010246, 0.051446,
+    0.058406, 0.031093, 0.033240, 0.008930, 0.000012, 0.000076, 0.007851, 0.003199
+]
+
+tabla_huffman = algoritmo_huffman(probs)
+cadena_bits,byte_array = codificar_en_byteArray("HOLA MUNDO"," ,.:;ABCDEFGHIJKLMNÑOPQRSTUVWXYZ",tabla_huffman)
+print("Cadena bits ",cadena_bits)
+print("Byte array ",byte_array)
+print("Compresion ",calcular_comprension("HOLA MUNDO",byte_array))
+print("redundancia y eficiencia ",calcularRedundanciaYEficiencia(probs, tabla_huffman))
+
+cadena_decodificada = decodificar_de_byteArray(cadena_bits," ,.:;ABCDEFGHIJKLMNÑOPQRSTUVWXYZ",tabla_huffman)
+print(cadena_decodificada)
+
+"""
+
+bytearray_cadena = codificar_usando_RCL("XXXYZZZZ")
+print("Compresion RCL ",calcular_comprension("XXXYZZZZ",bytearray_cadena))
+bytearray_cadena = codificar_usando_RCL("AAAABBBCCDAA")
+print("Compresion RCL ",calcular_comprension("AAAABBBCCDAA",bytearray_cadena))
+bytearray_cadena = codificar_usando_RCL("UUOOOOAAAIEUUUU")
+print("Compresion RCL ",calcular_comprension("UUOOOOAAAIEUUUU",bytearray_cadena))
