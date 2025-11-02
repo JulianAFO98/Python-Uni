fc9ed29277636acde3ab1a50aedf2257e116c2c1
julian <gonna1560@gmail.com>
2025-11-02 18:19:32 -0300

    terminada guia

--- Guia_4/canales.py
+++ Guia_4/canales.py
@@ -130,6 +130,16 @@ def mostrar_matriz_encuadrada(matriz, etiquetas_filas=None, etiquetas_columnas=N
 def info(num):
     return -math.log2(num)  if num !=0 else 0
 
+
+
+def calcular_entropia_salida(probs_priori, matriz_canal):
+    probs_priori = generar_probs_salida(probs_priori,matriz_canal)
+    entropia_salida = 0
+    for i in range(len(probs_priori)):
+        entropia_salida += probs_priori[i]*info(probs_priori[i])
+    print("Entropia de salida",round(entropia_salida,3))
+    return round(entropia_salida,3)
+
 def lista_entropias(probs_priori, matriz_canal):
     entropia_priori = 0
     lista_entropia_posteriori = []
@@ -147,6 +157,65 @@ def lista_entropias(probs_priori, matriz_canal):
         entropia_priori += probs_priori[i] * info(probs_priori[i])
 
     return round(entropia_priori, 3), lista_entropia_posteriori
+
+def calcular_equivocacion(probs_priori, matriz_canal):
+    # probs_priori = [P(a1), P(a2), ...]
+    # matriz_canal = [[P(b1|a1), P(b2|a1), ...],
+    #                 [P(b1|a2), P(b2|a2), ...], ...]
+
+    entro_priori, entropia_post = lista_entropias(probs_priori, matriz_canal)
+    probs_simultaneas = generar_matriz_eventos_simultaneos(probs_priori, matriz_canal)
+
+    # Calcular P(b) sumando columnas de P(a,b)
+    probs_b = [sum(col) for col in zip(*probs_simultaneas)]
+
+    # H(A|B) = sum_b P(b) * H(A|b)
+    H_A_dado_B = sum(pb * h for pb, h in zip(probs_b, entropia_post))
+
+    # H(B|A) = sum_a P(a) * H(B|a)
+    H_B_dado_A = 0
+    for pa, fila in zip(probs_priori, matriz_canal):
+        H_B_a = -sum(pba * math.log(pba, 2) for pba in fila if pba > 0)
+        H_B_dado_A += pa * H_B_a
+
+    
+    print(f"H(A|B) (equivocacion o ruido): {H_A_dado_B:.4f}")
+    print(f"H(B|A) (perdida): {H_B_dado_A:.4f}")
+
+    
+    return H_A_dado_B, H_B_dado_A # H(A|B) H(B|A)
+
+def calcular_entropia_afin(probs_priori, matriz_canal):
+    probs_simultaneas = generar_matriz_eventos_simultaneos(probs_priori, matriz_canal)
+    entropia_afin = 0
+    for i in range(len(probs_simultaneas)):
+        for j in range(len(probs_simultaneas[0])):
+            entropia_afin+= probs_simultaneas[i][j] * info(probs_simultaneas[i][j])
+    print("Entropia Afin: ",entropia_afin)
+    return entropia_afin
+
+def calcular_informacion_mutua(probs_priori, matriz_canal):
+    # probs_simultaneas[i][j] = P(A=i, B=j)
+    probs_simultaneas = generar_matriz_eventos_simultaneos(probs_priori, matriz_canal)
+    
+    n = len(probs_simultaneas)
+    m = len(probs_simultaneas[0])
+    
+    # Probabilidades marginales
+    P_A = [sum(probs_simultaneas[i][j] for j in range(m)) for i in range(n)]
+    P_B = [sum(probs_simultaneas[i][j] for i in range(n)) for j in range(m)]
+    
+    # Informacion mutua
+    info_mutua = 0
+    for i in range(n):
+        for j in range(m):
+            if probs_simultaneas[i][j] > 0: 
+                info_mutua += probs_simultaneas[i][j] * math.log2(probs_simultaneas[i][j] / (P_A[i] * P_B[j]))
+    print("Informacion mutua: ",info_mutua)
+    return info_mutua
+
+
+
 #print(generar_matriz_canal("abcacaabbcacaabcacaaabcaca","01010110011001000100010011"))
 
 #Punto 3
@@ -205,7 +274,7 @@ entropia_priori,entropia_posteriori = lista_entropias([0.3,0.3,0.4],[[0.4,0.4,0.
 print(entropia_priori)
 print(entropia_posteriori)
 """
-
+"""
 entropia_priori,entropia_posteriori = lista_entropias([0.14,0.52,0.34],[[0.5,0.3,0.2],[0,0.4,0.6],[0.2,0.8,0]])
 entropia_priori,entropia_posteriori = lista_entropias([0.25,0.25,0.5],[[0.25,0.25,0.25,0.25],[0.25,0.25,0,0.5],[0.5,0,0.5,0]])
 entropia_priori,entropia_posteriori = lista_entropias([0.12,0.24,0.14,0.5],[[0.25,0.15,0.3,0.3],[0.23,0.27,0.25,0.25],[0.1,0.4,0.25,0.25],[0.34,0.26,0.2,0.2]])
@@ -213,3 +282,12 @@ entropia_priori,entropia_posteriori = lista_entropias([0.12,0.24,0.14,0.5],[[0.2
 
 print(entropia_priori)
 print(entropia_posteriori)
+
+"""
+
+entro_priori,entro_posteriori = lista_entropias([0.14,0.52,0.34],[[0.5,0.3,0.2],[0,0.4,0.6],[0.2,0.8,0]])
+print(entro_priori)
+calcular_entropia_salida([0.14,0.52,0.34],[[0.5,0.3,0.2],[0,0.4,0.6],[0.2,0.8,0]])
+calcular_equivocacion([0.14,0.52,0.34],[[0.5,0.3,0.2],[0,0.4,0.6],[0.2,0.8,0]])
+calcular_entropia_afin([0.14,0.52,0.34],[[0.5,0.3,0.2],[0,0.4,0.6],[0.2,0.8,0]])
+calcular_informacion_mutua([0.14,0.52,0.34],[[0.5,0.3,0.2],[0,0.4,0.6],[0.2,0.8,0]])
